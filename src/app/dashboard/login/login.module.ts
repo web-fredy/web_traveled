acd4ec43461f943c3b12273856e274f54fdc4996
Fredy Gildardo Garcia <fredy.ggm15@gmail.com>
2020-04-04 12:47:56 -0600

    Login

--- src/app/dashboard/login/login.module.ts
+++ src/app/dashboard/login/login.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class LoginModule { }
