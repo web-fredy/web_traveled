d5c2bf73d49694271f5a4ce855d4209892bf7950
Fredy Gildardo Garcia Mendoza <fredy.ggm15@gmail.com>
2020-04-04 18:05:26 -0600

    Mejoras generales

--- src/app/share/home/home.component.ts
+++ src/app/share/home/home.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-// declare function plugin_home(): any;
+declare function plugin_home(): any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [
   ],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
-    // plugin_home();
+    plugin_home();
+  }
+
+  ngOnDestroy(): void {
+    plugin_home();
   }
 
 }
---
+++ src/assets/js/home/main.js
@@ -0,0 +1,19 @@
+function plugin_home() {
+  (function($) {
+    "use strict";
+    var $navbar = $(".myMenu"), y_pos = $navbar.offset().top, height = $navbar.height();
+    //scroll top 0 sticky
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 0) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");
+        }
+    });
+  })(jQuery, undefined);
+
+  $(".btn-menu").click(function(){
+    $("#myMenu").toggleClass("open");
+  });
+}
