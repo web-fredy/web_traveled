cbe11d716934a994313dd660a281c384e4a993b4
Fredy Gildardo Garcia <fredy.ggm15@gmail.com>
2020-04-06 13:18:47 -0500

    Up

--- src/assets/js/home/main.js
+++ src/assets/js/home/main.js
@@ -1,19 +1,49 @@
 function plugin_home() {
   (function($) {
     "use strict";
-    var $navbar = $(".myMenu"), y_pos = $navbar.offset().top, height = $navbar.height();
+  
+    // Back to top button
+    // $(window).scroll(function() {
+    //   if ($(this).scrollTop() > 100) {
+    //     $('.back-to-top').fadeIn('slow');
+    //   } else {
+    //     $('.back-to-top').fadeOut('slow');
+    //   }
+    // });
+    // $('.back-to-top').click(function() {
+    //   $('html, body').animate({
+    //     scrollTop: 0
+    //   }, 1500, 'easeInOutExpo');
+    //   return false;
+    // });
+  
+    // Navbar scroll class
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 80) {
+        $('#navbar').addClass('gradient');
 
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-          $navbar.addClass("sticky");
-        } else {
-          $navbar.removeClass("sticky");
-        }
+        // $('#navbar').addClass('navbar-dark');
+      } else {
+        $('#navbar').removeClass('gradient');
+        
+        // $('#navbar').removeClass('navbar-dark');
+      }
     });
-  })(jQuery, undefined);
-
-  $(".btn-menu").click(function(){
-    $("#myMenu").toggleClass("open");
-  });
+  
+    if ($(window).scrollTop() > 80) {
+      $('#navbar').addClass('gradient');
+    }
+    
+    // Hamburguer menu
+    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+  
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+  })(jQuery);
 }
