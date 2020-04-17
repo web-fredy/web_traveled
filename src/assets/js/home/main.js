b9d93f846016b9d0e273536ed71e3a7009552f7e
Fredy Gildardo Garcia Mendoza <fredy.ggm15@gmail.com>
2020-04-16 22:16:39 -0500

    Mejoras generales

--- src/assets/js/home/main.js
+++ src/assets/js/home/main.js
@@ -1,7 +1,7 @@
 function plugin_home() {
   (function($) {
     "use strict";
-  
+
     // Back to top button
     // $(window).scroll(function() {
     //   if ($(this).scrollTop() > 100) {
@@ -16,7 +16,7 @@ function plugin_home() {
     //   }, 1500, 'easeInOutExpo');
     //   return false;
     // });
-  
+
     // Navbar scroll class
     $(window).scroll(function() {
       if ($(this).scrollTop() > 150) {
@@ -25,14 +25,14 @@ function plugin_home() {
         $('#navbar').addClass('py-4');
       }
     });
-  
+
     if ($(window).scrollTop() > 150) {
       $('#navbar').addClass('py-4');
     }
-    
+
     // Hamburguer menu
     var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-  
+
     var hamburgers = document.querySelectorAll(".hamburger");
     if (hamburgers.length > 0) {
       forEach(hamburgers, function(hamburger) {
@@ -42,4 +42,6 @@ function plugin_home() {
       });
     }
   })(jQuery);
+
+  AOS.init();
 }
